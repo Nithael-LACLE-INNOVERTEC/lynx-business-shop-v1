586a54557128ff9137d59ad0c6c4c40d719e43f8
Joseph Holonou <yao.holonou@hotmail.de>
2025-03-27 11:26:57 +0000

    Creare .env file

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ const Home = () => {
       <div className="px-4 py-3 md:px-6 lg:px-6 xl:px-10">
         <h1 className="text-2xl font-bold">Home</h1>
         <p>Manage your Home here.</p>
+        <p>`{import.meta.env.VITE_API_BASE_URL}`</p>
       </div>
     );
   };
